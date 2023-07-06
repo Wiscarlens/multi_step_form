10bcce58d6e8c30c15422bb6098f21f94856748b
Wiscarlens <wiscarlens@gmail.com>
2023-07-06 10:08:34 -0400

    - Add Save button

--- app/src/main/java/com/example/stepperform/MainActivity.java
+++ app/src/main/java/com/example/stepperform/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.stepperform;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.res.ColorStateList;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton step1Button;
@@ -37,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText firstName;
     private TextInputEditText middleName;
     private TextInputEditText lastName;
+    private TextInputLayout DOB_layout;
     private TextInputEditText DOB;
     private TextInputEditText gender;
 
@@ -88,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         firstName = stepOneLayout.findViewById(R.id.signupFirstNameText);
         middleName = stepOneLayout.findViewById(R.id.signupMiddleNameText);
         lastName = stepOneLayout.findViewById(R.id.signupLastNameText);
+        DOB_layout = stepOneLayout.findViewById(R.id.signupDOBLayout);
         DOB = stepOneLayout.findViewById(R.id.signupDOBText);
         gender = stepOneLayout.findViewById(R.id.signupGenderText);
 
@@ -104,6 +118,16 @@ public class MainActivity extends AppCompatActivity {
         position = stepThreeLayout.findViewById(R.id.signupPositionText);
         password = stepThreeLayout.findViewById(R.id.signupPasswordText);
         confirmedPassword = stepThreeLayout.findViewById(R.id.signupConfirmPasswordText);
+
+
+        // When user click Calendar icon
+        DOB_layout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDate();
+            }
+        });
+
 
         // Progress bar default value
         progressBar.setProgress(33);
@@ -147,9 +171,9 @@ public class MainActivity extends AppCompatActivity {
             step = 1;
         }
 
-        else if(step > 3) {
-            step = 3;
-        }
+//        else if(step > 3) {
+//            step = 3;
+//        }
 
         // Set active button background
         switch (step) {
@@ -258,6 +282,54 @@ public class MainActivity extends AppCompatActivity {
                 nextButton.setColorFilter(colorBlue);
 
                 break;
+
+            case 4:
+//                Toast.makeText(this, firstName.getText(), Toast.LENGTH_SHORT).show();
+                showDialogMessage();
+
+                break;
         }
+
+    }
+
+    private void selectDate() {
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+            // When the user selects a date, format it as a string and set it as the text of the EditText
+            calendar.set(year, month, dayOfMonth);
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+            String formattedDate = dateFormat.format(calendar.getTime());
+
+            DOB.setText(formattedDate);
+        };
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create and show the date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                MainActivity.this,
+                dateSetListener,
+                year,
+                month,
+                dayOfMonth);
+        datePickerDialog.show();
+    }
+
+    private void showDialogMessage() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Confirm")
+                .setMessage("Do you want to create a new user?")
+                .setNegativeButton("No", (dialog, which) -> {
+                    // If user click on NO nothing happen
+                })
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    // Open ConfirmationFragment when user click on YES
+
+                }).show();
     }
 }
